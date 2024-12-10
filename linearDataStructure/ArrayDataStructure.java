9f661b3f742a27ca9d6a7bb031b7ec4fd424f08b
jeewan <jeewantmgr@gmail.com>
2024-12-10 09:36:27 +0545

    Adding displayStringArray

--- linearDataStructure/ArrayDataStructure.java
+++ linearDataStructure/ArrayDataStructure.java
@@ -8,22 +8,30 @@ public class ArrayDataStructure {
 
     public static <T> void initializedAray(){
        int[] data = {1,5,3,6,8,9,};
-        displayArray(data);
+        displayIntArray(data);
         System.out.println("#############################");
         int[] dataOfString = new int[3];
         dataOfString[0] = 1;
         dataOfString[1] = 2;
         dataOfString[2] = 3;
-        displayArray(dataOfString);
+        displayIntArray(dataOfString);
 
         String[] dataOfStringArray = new String[3];
         dataOfStringArray[0] = "Jeewan Thapa Magar";
         dataOfStringArray[1] = "";
         dataOfStringArray[2] = "3";
+        displayStrinArray(dataOfStringArray);
+
     }
 
-    public static void displayArray(int[] data) {
+    public static void displayIntArray(int[] data) {
         for(int a: data) {
+            System.out.println(a);
+        }
+    }
+
+    public static void displayStrinArray(String[] data) {
+        for(String a: data) {
             System.out.println(a);
         }
     }
